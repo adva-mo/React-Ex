08cf07090abba87b075866e93f3c02f668d24f2c
adva-mo <advamozes@gmail.com>
2022-11-07 14:49:08 +0200

    EX 6.3

--- ex 5.3/src/components/Mother.js
+++ ex 5.3/src/components/Mother.js
@@ -12,13 +12,21 @@ const Mother = (props) => {
   return (
     <div className="mother">
       <h1>{props.title}</h1>
-      <Child {...kids[0]}></Child>
+      {kids.map(kid=>{
+        <Child {kid}/>
+      })}
+      {/* <Child {...kids[0]}></Child>
       <Child {...kids[1]}></Child>
       <Child {...kids[2]}></Child>
       <Child {...kids[3]}></Child>
-      <Child {...kids[4]}></Child>
+      <Child {...kids[4]}></Child> */}
     </div>
   );
 };
 
 export default Mother;
+
+//! rendering elements dinamically
+//* when jsx encount array of elements, itll render it one by another
+//* so to render same component with deiffernet data each time we will use map method which returns an array of elements to render
+//* we retransform an array of data into array of objects
