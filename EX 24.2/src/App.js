47e9d66fc180ecafb2cdb174e3118b89c1e7847e
adva-mo <advamozes@gmail.com>
2022-11-23 09:51:06 +0200

    EX 24.2

---
+++ EX 24.2/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./App.css";
+import Backwards from "./components/Backwards";
+
+function App() {
+  return (
+    <div>
+      http costum hooks
+      <Backwards />
+    </div>
+  );
+}
+
+export default App;
