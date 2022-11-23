a3d11f6df6a7806b639ff85d5dedcfacf93021ea
adva-mo <advamozes@gmail.com>
2022-11-23 12:42:51 +0200

    ex 21.2

--- copy/src/App.js
+++ copy/src/App.js
@@ -1,28 +1,48 @@
+import axios from "axios";
 import React from "react";
 import { Component } from "react";
 import "./App.css";
-
-function Box() {
-  return <div className="box"></div>;
-}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      isBoxShows: false,
+      countries: [],
+      display: [],
     };
   }
-  componentDidMount() {
-    const timer = setTimeout(() => {
-      this.setState({ isBoxShows: true });
-    }, 1000);
-    return () => {
-      clearTimeout(timer);
-    };
+  async componentDidMount() {
+    try {
+      const response = await axios.get("https://restcountries.com/v2/all");
+      console.log(response.data);
+      this.setState((prev) => {
+        return { countries: [...response.data], display: [...response.data] };
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  handleChange({ target }) {
+    console.log(target.value);
+    const filtered = this.state.countries.filter((c) => {
+      return c.name.startsWith(target.value);
+    });
+    console.log(filtered);
+    this.setState((prev) => {
+      if (target.value === "") return { display: [...this.state.countries] };
+      else return { display: [...filtered] };
+    });
   }
   render() {
-    return <>{this.state.isBoxShows && <Box />}</>;
+    return (
+      <>
+        <input onChange={this.handleChange.bind(this)} />
+        {this.state.countries.length > 1 &&
+          this.state.display.map((c) => {
+            return <li>{c.name}</li>;
+          })}
+      </>
+    );
   }
 }
 export default App;
